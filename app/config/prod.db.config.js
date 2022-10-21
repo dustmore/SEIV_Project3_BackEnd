06b7571455d1b75cd76062c614ceae86e80771c6
JuliaFlores21 <julia.flores@eagles.oc.edu>
2022-10-21 11:16:56 -0500

    Section Controller and prod db files

---
+++ app/config/prod.db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: "localhost",
+    port: 3306,
+    USER: "t42022",
+    PASSWORD: "cs@oc2022t4",
+    DB: "p3-t4-courses",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
---
+++ app/controllers/section.controller.js
@@ -0,0 +1,161 @@
+const db = require("../models");
+const Section = db.section;
+const Op = db.Sequelize.Op;
+// Create and Save a new section
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+  // Create a section
+  const section = {
+    sectionId: req.body.sectionId,
+    courseId: req.body.courseId,
+    sectionStartDate: req.body.sectionStartDate,
+    sectionEndDate: req.body.sectionEndDate,
+    sectionStartTime: req.body.sectionStartTime,
+    sectionEndTime: req.body.sectionEndTime
+  };
+  // Save section in the database
+  Section.create(section)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the section."
+      });
+    });
+};
+// Retrieve all sections from the database.
+exports.findAll = (req, res) => {
+  const sectionId = req.query.sectionId;
+  var condition = sectionId ? { sectionId: { [Op.like]: `%${sectionId}%` } } : null;
+  Section.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving sections."
+      });
+    });
+};
+// Find a single section with an id
+exports.findAllForUser = (req, res) => {
+  const userId = req.params.userId;
+  Section.findAll({ where: { userId : userId }})
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find section with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving section with id=" + id
+      });
+    });
+};
+// Find a single section with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  Section.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find section with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving section with id=" + id
+      });
+    });
+};
+// Update a section by the id in the request
+exports.update = (req, res) => {
+  const id = req.params.id;
+  Section.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "section was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update section with id=${id}. Maybe section was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating section with id=" + id
+      });
+    });
+};
+// Delete a section with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Section.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Section was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Section with id=${id}. Maybe Section was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Section with id=" + id
+      });
+    });
+};
+// Delete all Sections from the database.
+exports.deleteAll = (req, res) => {
+  Section.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Sections were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all Sections."
+      });
+    });
+};
+// Find all published Sections
+exports.findAllPublished = (req, res) => {
+  Section.findAll({ where: { published: true } })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Sections."
+      });
+    });
+};
