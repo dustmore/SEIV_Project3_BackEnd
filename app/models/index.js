f789e4926316307cd2caf11e8e938690ebea75a7
JuliaFlores21 <julia.flores@eagles.oc.edu>
2022-10-23 19:37:49 -0500

    Edited course, section & index model

--- app/models/index.js
+++ app/models/index.js
@@ -17,20 +17,15 @@ db.sequelize = sequelize;
 
 db.user = require("./user.model.js")(sequelize, Sequelize);
 db.course = require("./course.model.js")(sequelize, Sequelize);
-db.session = require("./session.model.js")(sequelize, Sequelize);
-db.tutorial = require("./tutorial.model.js")(sequelize, Sequelize);
-db.lesson = require("./lesson.model.js")(sequelize, Sequelize);
+db.section = require("./section.model.js")(sequelize, Sequelize);
 
-// foreign key for session
-db.user.hasMany(db.session, { as: 'session'}, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
-db.session.belongsTo(db.user, { as: 'user'}, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
+// foreign key for section
+db.course.hasMany(db.section, { as: 'section'}, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
+db.section.belongsTo(db.course, { as: 'course'}, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
 
 // foreign key for tutorials
-db.user.hasMany(db.tutorial, { as: 'tutorial'}, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
-db.tutorial.belongsTo(db.user, { as: 'user'}, { foreignKey: { allowNull: false }, onDelete: 'CASCADE', });
+//db.user.hasMany(db.tutorial, { as: 'tutorial'}, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
+//db.tutorial.belongsTo(db.user, { as: 'user'}, { foreignKey: { allowNull: false }, onDelete: 'CASCADE', });
 
-// foreign key for lessons
-db.tutorial.hasMany(db.lesson, { as: 'lesson' }, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
-db.lesson.belongsTo(db.tutorial, { as: 'tutorial' }, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
 
 module.exports = db;
