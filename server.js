4ff7056c1ad744e94678a0f5bd1abdb1ac848a4f
JuliaFlores21 <julia.flores@eagles.oc.edu>
2023-02-19 23:23:59 -0600

    change route to wrong

--- server.js
+++ server.js
@@ -39,47 +39,10 @@ require("./app/routes/semester.routes")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3014;
+const PORT = process.env.PORT || 3100;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-/*
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
- 
-
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-app.use(cors(corsOptions));
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const db = require("./app/models");
-
-db.sequelize.sync()
-  .then(() => {
-    console.log("Synced db.");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
-
-require("./app/routes/course.routes")(app);
-// set port, listen for requests
-const PORT = process.env.PORT || 3014;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-*/
 
